73abe397ec90b40686bc88926eaa74c4fb60de52
tanupat <tanupat.j@hotmail.com>
2024-02-12 23:08:41 +0700

    Login Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,28 @@ const config: Config = {
         'secondary-gray': '#E9E9E9',
         'disable': '#A5A5A5'
       },
+      fontFamily: {
+        kanit: "Kanit",
+        inherit: "inherit",
+        "p6-prompt-reg-12": "Prompt",
+      },
+      borderRadius: {
+        "11xl": "30px",
+        "8xs": "5px",
+        xl: "20px",
+        "3xs": "10px",
+      },
+    },
+    fontSize: {
+      xs: "0.75rem",
+      base: "1rem",
+      lg: "1.13rem",
+      "11xl": "1.88rem",
+      "5xl": "1.5rem",
+      smi: "0.81rem",
+      "3xs": "0.63rem",
+      inherit: "inherit",
+
     },
   },
   plugins: [],
