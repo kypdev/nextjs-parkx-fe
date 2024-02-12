b64bcbdf5b47eb8dfb8a18c96634697795a28823
kypdev <kyptdev@gmail.com>
2024-02-12 19:20:55 +0700

    Add lib and schemas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'primary-blue': '#1C355E',
+        'primary-green': '#7FBC00',
+        'secondary-blue': '#343434',
+        'secondary-gray': '#E9E9E9',
+        'disable': '#A5A5A5'
+      },
     },
   },
   plugins: [],
