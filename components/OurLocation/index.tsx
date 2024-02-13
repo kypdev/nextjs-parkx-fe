49e2c51704d6329985d990b3f4bc8faccf45c195
NSuksamran <hrisdev@MacBook-Pro-khxng-HRISDev.local>
2024-02-14 00:17:03 +0700

    Revert "Revert "Update responsive""
    This reverts commit d57517455715c818c0d24ba6d96fc2a8e3fba195.

--- components/OurLocation/index.tsx
+++ components/OurLocation/index.tsx
@@ -1,62 +1,74 @@
-import React from 'react'
-import { ChevronLeft, ChevronRight, MapPin } from 'lucide-react'
-import Image from 'next/image'
+import React from "react";
+import { ChevronLeft, ChevronRight, MapPin } from "lucide-react";
+import Image from "next/image";
 
 const OurLocation = () => {
   const onClickLeft = () => {
-    console.log('left')
-  }
+    console.log("left");
+  };
   const onClickRight = () => {
-    console.log('right')
-  }
+    console.log("right");
+  };
   return (
     <>
-      <div className='relative'>
-        <div className='flex flex-row bg-primary-blue px-10 py-10 mt-10 mb-8 '>
-          <MapPin color='white' size={48} />
-          <h2 className='text-white text-3xl text-bold flex items-center'>
-            OUR LOCATIONS
-          </h2>
-        </div>
-        <div className='flex flex-col'>
-          <div className='flex flex-row pl-16'>
-            <Image
-              src={'/images/parkx-logo.png'}
-              alt='Picture of the author'
-              // sizes='100vw'
-              // style={{
-              //   width: '100%',
-              //   height: 'auto',
-              // }}
-              width={219}
-              height={65}
-            />
-            <div className='flex justify-center text-center bg-primary-green px-16 my-3 ml-4 text-white'>
-              <p className='flex items-center	text-lg'>อุดมสุข</p>
+      <div className="relative grid grid-cols-2 ">
+        {/* px-10 py-10 mt-10 mb-8  */}
+        <div className="col-span-2 md:col-span-1">
+          {/* <div className="h-[40%] w-[100%] "></div> */}
+          <div className="flex flex-row pl-2 py-2 bg-primary-blue">
+            <MapPin color="white" size={48} />
+            <h2 className="text-white text-xl md:text-[2rem] text-bold flex items-center">
+              OUR LOCATIONS
+            </h2>
+          </div>
+          <div className="grid grid-cols-2 flex-row mt-2">
+            <div className="col-span-1 w-[50%] h-[50%] md:w-[100%] md:h-[100%] pl-4 md:pl-16">
+              <Image
+                src={"/images/parkx-logo.png"}
+                alt="Picture of the author"
+                // sizes='100vw'
+                // style={{
+                //   width: '100%',
+                //   height: 'auto',
+                // }}
+                width={219}
+                height={65}
+              />
+            </div>
+            <div className="col-span-1 flex justify-center text-center bg-primary-green px:0 md:px-16 my:0 md:my-3 mr-4 text-white">
+              <p className="flex items-center	text-lg">อุดมสุข</p>
             </div>
           </div>
-          <p className='flex items-center	text-lg pl-16 mt-16'>
+          <p className="flex items-center	text-lg pl-16 mt-4">
             2784 Sukhumvit 66/1 Khwaeng
             <br /> Banmg Na, Bang Na
             <br /> Bangkok 10260 PENTA 591
           </p>
-          <p className='flex text-primary-green items-center	text-lg pl-16 mt-16'>
+          <p className="flex text-primary-green items-center	text-lg pl-16 mt-4 mb-4 md:mb-0">
             อ่านรายละเอียดเพิ่มเติม
           </p>
-          <div className='absolute top-4 right-12'>
+        </div>
+        <div className="col-span-2 md:col-span-1 flex flex-col ">
+          <div className="relative md:absolute top-0 ">
             <Image
-              src={'/images/home/content-slide.png'}
-              alt='Picture of the author'
+              src={"/images/home/content-slide.png"}
+              alt="Picture of the author"
               width={554}
               height={420}
             />
           </div>
-          <ChevronLeft onClick={onClickLeft} className='absolute top-48' />
-          <ChevronRight onClick={onClickRight} className='absolute top-48 right-0' />
+          <ChevronLeft
+            onClick={onClickLeft}
+            className="hidden absolute top-48"
+          />
+          <ChevronRight
+            onClick={onClickRight}
+            className="hidden absolute top-48 right-0"
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default OurLocation
+export default OurLocation;
--- components/navbar/components/menubar.tsx
+++ components/navbar/components/menubar.tsx
@@ -1,57 +1,93 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 const Menubar = () => {
   return (
-    <header className='w- flex flex-row items-start justify-start py-[0rem] pr-[2.38rem] pl-[1.25rem] box-border max-w-full text-left text-[1.25rem] text-darkslategray-200 font-kanit'>
-      <div className='flex-1 flex flex-row items-center justify-start gap-[2rem] max-w-full mq750:gap-[1rem]'>
-        <Link href={'/'}>
+    <header className="w-screen flex flex-row items-center justify-center py-[0rem] pr-0 pl-0 md:pr-[2.38rem] md:pl-[1.25rem] box-border max-w-full text-left text-[1.25rem] text-darkslategray-200 font-kanit">
+      <div className="h-[20%] mx-2 flex-1 flex flex-row items-center justify-center gap-[2rem] max-w-full mq750:gap-[1rem]">
+        {/* <Link href={"/"}>
           <Image
-            src={'/images/parkx-logo.png'}
-            alt={''}
+            src={"/images/parkx-logo.png"}
+            alt={""}
             width={240}
             height={72}
           />
-        </Link>
-        <div className='flex-1 flex flex-col items-start justify-start pt-[0rem] pb-[0.38rem] pr-[2.5rem] pl-[0rem] box-border max-w-full md:w-full '>
-          <div className='self-stretch flex flex-row items-start justify-start gap-[4.13rem] mq450:gap-[1rem] mq750:gap-[2.06rem] mq1050:hidden'>
-            <div className='h-[1.88rem] relative font-semibold inline-block whitespace-nowrap'>
-              <Link href={'/about'}>ABOUT PARK X</Link>
-            </div>
-            <div className='flex-[0.9412] flex flex-col items-start justify-start py-[0rem] pr-[0.75rem] pl-[0rem]'>
-              <div className='h-[1.88rem] relative font-semibold inline-block whitespace-nowrap'>
-                <Link href={'/parking-solution'}>PARKING SOLUTIONS</Link>
+        </Link> */}
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-6 md:col-span-1 w-[50%] h-[50%] md:w-[100%] md:h-[50%]">
+            <Link href={"/"}>
+              <Image
+                src={"/images/parkx-logo.png"}
+                alt={""}
+                width={240}
+                height={72}
+              />
+            </Link>
+          </div>
+          <div className="col-span-3 md:col-span-1 text-[70%]">
+            <Link href={"/about"}>ABOUT PARK X</Link>
+          </div>
+          <div className="col-span-3 md:col-span-1 text-[70%]">
+            <Link href={"/parking-solution"}>PARKING SOLUTIONS</Link>
+          </div>
+          <div className="col-span-3 md:col-span-1 text-[70%]">
+            <Link href={"/#"}>NEWS & PROMOTIONS</Link>
+          </div>
+          <div className="col-span-3 md:col-span-1 text-[70%]">
+            <Link href={"/#"}>CONTACT US</Link>
+          </div>
+          <div className="col-span-6 md:col-span-1 ">
+            {/* <div className=" bg-primary-blue flex flex-row items-end justify-start pt-[0.69rem] pb-[0.63rem] pr-[0.44rem] pl-[1.25rem] gap-[0.81rem]"> */}
+            <div className="grid grid-cols-2 divide-x-2 md:divide-y-2 md:divide-x-0 p-1 rounded-lg bg-primary-blue">
+              <div className="col-span-1 md:col-span-2 text-base text-white hover:underline text-center">
+                <Link href="/register">สมัครสมาชิก</Link>{" "}
+              </div>
+              {/* <div className="col-span-1"></div> */}
+              <div className="col-span-1 md:col-span-2 text-base text-white hover:underline text-center">
+                <Link href="/register">เข้าสู่ระบบ</Link>{" "}
               </div>
             </div>
-            <div className='h-[1.88rem] flex-1 relative font-semibold inline-block whitespace-nowrap'>{`NEWS & PROMOTIONS`}</div>
           </div>
         </div>
-        <div className='flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem]'>
-          <div className='h-[1.88rem] relative font-semibold inline-block whitespace-nowrap'>
+        {/* <div className="flex-1 flex flex-col items-start justify-start pt-[0rem] pb-[0.38rem] pr-[2.5rem] pl-[0rem] box-border max-w-full md:w-full ">
+          <div className="self-stretch flex flex-row items-start justify-start gap-[4.13rem] mq450:gap-[1rem] mq750:gap-[2.06rem] mq1050:hidden">
+            <div className="h-[1.88rem] relative font-semibold inline-block whitespace-nowrap">
+              <Link href={"/about"}>ABOUT PARK X</Link>
+            </div>
+            <div className="flex-[0.9412] flex flex-col items-start justify-start py-[0rem] pr-[0.75rem] pl-[0rem]">
+              <div className="h-[1.88rem] relative font-semibold inline-block whitespace-nowrap">
+                <Link href={"/parking-solution"}>PARKING SOLUTIONS</Link>
+              </div>
+            </div>
+            <div className="h-[1.88rem] flex-1 relative font-semibold inline-block whitespace-nowrap">{`NEWS & PROMOTIONS`}</div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem]">
+          <div className="h-[1.88rem] relative font-semibold inline-block whitespace-nowrap">
             CONTACT US
           </div>
         </div>
-        <div className='flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem]'>
-          <div className='h-[1.88rem] relative font-semibold inline-block whitespace-nowrap'>
+        <div className="flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem]">
+          <div className="h-[1.88rem] relative font-semibold inline-block whitespace-nowrap">
             {/* <Button shape="circle" className='bg-[#1da1f2]'>Signin | Signup</Button> */}
-          </div>
-        </div>
+        {/* </div>
+        </div> */}
       </div>
 
-      <div className='flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem] text-[0.81rem] text-white mt-4'>
-        <div className='rounded-xl bg-primary-blue flex flex-row items-end justify-start pt-[0.69rem] pb-[0.63rem] pr-[0.44rem] pl-[1.25rem] gap-[0.81rem]'>
-          <div className='relative font-medium z-[1] hover:underline'>
-            <Link href='/register'>สมัครสมาชิก</Link>{' '}
+      {/* <div className="flex flex-col items-start justify-start pt-[0rem] px-[0rem] pb-[0.38rem] text-[0.81rem] text-white mt-4">
+        <div className="rounded-xl bg-primary-blue flex flex-row items-end justify-start pt-[0.69rem] pb-[0.63rem] pr-[0.44rem] pl-[1.25rem] gap-[0.81rem]">
+          <div className="relative font-medium z-[1] hover:underline">
+            <Link href="/register">สมัครสมาชิก</Link>{" "}
           </div>
           |
-          <div className='relative font-medium z-[1] hover:underline'>
-            {' '}
-            <Link href='/register'>เข้าสู่ระบบ</Link>{' '}
+          <div className="relative font-medium z-[1] hover:underline">
+            {" "}
+            <Link href="/register">เข้าสู่ระบบ</Link>{" "}
           </div>
         </div>
-      </div>
+      </div> */}
     </header>
-  )
-}
+  );
+};
 
-export default Menubar
+export default Menubar;
